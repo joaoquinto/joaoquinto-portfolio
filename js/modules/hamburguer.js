cd64e2024aecd651629b68ff0801e1e8e3b2e10a
Joaoquinto <joaovictoroliveirapereira6522@gmail.com>
2022-02-14 08:43:30 -0300

    Issue #27, corrigindo erro na centralização vertical do header

--- js/modules/hamburguer.js
+++ js/modules/hamburguer.js
@@ -1,18 +1,30 @@
 const buttonMenu = document.querySelector('#menu-hamburguer')
 const menuHamburguerItem = document.querySelector('.menu-hamburguer-item')
-export default function menuHamburguer() {
+
+export  function menuHamburguer() {
 const validator = menuHamburguerItem.classList.contains('ativo')
-
-
-if (validator != true) {
-  menuHamburguerItem.classList.add('ativo')
-  menuHamburguerItem.ariaExpanded = 'true'
-} else {
-  menuHamburguerItem.classList.remove('ativo')
-  menuHamburguerItem.ariaExpanded = 'false'
-}
-
+  if (validator != true) {
+    menuHamburguerItem.classList.add('ativo')
+    menuHamburguerItem.ariaExpanded = 'true'
+  } else {
+    menuHamburguerItem.classList.remove('ativo')
+    menuHamburguerItem.ariaExpanded = 'false'
+  }
 }
 
 buttonMenu.addEventListener('click',menuHamburguer)
+
+export function acessibilidade(){
+  if (document.body.clientWidth < 1000) {
+    menuHamburguerItem.ariaOrientation = 'vertical'
+    menuHamburguerItem.ariaExpanded = 'true'
+    buttonMenu.ariaHidden = 'false'
+  }else{
+    menuHamburguerItem.ariaOrientation = 'horizontal'
+    menuHamburguerItem.ariaExpanded = 'true'
+    buttonMenu.ariaHidden = 'true'
+  }
+}
+
+window.onresize = acessibilidade
 
